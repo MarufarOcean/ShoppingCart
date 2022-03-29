1f94ef27f65f4230014b0efe12139e885dacabac
Md. Marufar Rahman <Md. Marufar Rahman@MARUFAR>
2022-03-30 00:00:22 +0600

    Add project files.

---
+++ ShoppingCart.DataAccess/ViewModels/CategoryVM.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using ShoppingCart.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingCart.DataAccess.ViewModels
+{
+    public class CategoryVM
+    {
+        public Category Category { get; set; } = new Category();
+        [ValidateNever]
+        public IEnumerable<Category> categories { get; set; } = new List<Category>(); 
+    }
+
+   
+}
